7b11931cf7fa777ce10a3052c8c893d49b8bd442
Jan0Fu <115988224+Jan0Fu@users.noreply.github.com>
2023-03-31 12:03:40 +0200

    refactor: optimize resource initialization

--- src/cinema/SeatRepository.java
+++ src/cinema/SeatRepository.java
@@ -8,8 +8,16 @@ import java.util.List;
 @Repository
 public class SeatRepository {
 
-    private List<Seat> availableSeats = new ArrayList<>();
+    private List<Seat> availableSeats;
 
+    public SeatRepository() {
+        availableSeats = new ArrayList<>();
+        for (int i = 1; i < 10; i++) {
+            for (int j = 1; j < 10; j++) {
+                availableSeats.add(new Seat(i, j));
+            }
+        }
+    }
     public void createList() {
         for (int i = 1; i < 10; i++) {
             for (int j = 1; j < 10; j++) {
@@ -19,9 +27,6 @@ public class SeatRepository {
     }
 
     public List<Seat> getSeats() {
-        if (availableSeats.isEmpty()) {
-            createList();
-        }
         return availableSeats;
     }
 }
